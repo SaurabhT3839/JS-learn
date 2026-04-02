719a919b6db0f609539fe2c91d5537f3a017bc33
saurabh <saurabh.thakur@ditstek.com>
2026-04-02 16:03:12 +0530

    Add JavaScript files for data types and conversion operations

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,46 @@
+let value = 3
+let negativeValue = -value
+
+console.log(value);
+console.log(negativeValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2); // 2 to the power 2
+
+
+let str1 = "Hello"
+let str2 = "World"
+
+let str3 = str1 + " " + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+// let num1, num2, num3
+
+
+let gameCounter = 100
+gameCounter;
+++gameCounter;
+
+console.log(gameCounter);
+console.log(typeof gameCounter);
+
+
+
+// Prefix &  Postfix 
