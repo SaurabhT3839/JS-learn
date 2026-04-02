88ca0040f5b886c805a135b641ccd8f765943783
saurabh <saurabh.thakur@ditstek.com>
2026-04-02 15:27:58 +0530

    Add initial JavaScript files for data types and variables

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// alert("Hello World!");
+
+console.log("Hello World!");
+
+let name = "Saurabh";
+let age = 24;
+let isLoggedIn = false; //true or false
+let state; //undefined
+
+
+
+
+// Premitedive data types in JavaScript are:
+
+    // number => 2 to power 53
+    // bigint => greater than 2 to power 53
+    // string => "Saurabh" 'Saurabh' `Saurabh` ""
+    // boolean => true or false
+    // undefined => variable is declared but not assigned a value
+    // null => variable is declared and assigned a value of null
+    // symbol => unique identifier
+
+//  null is Object
+// undefined is undefined
+
+//objects => non-primitive data types in JavaScript are:
+
+    // object => {name: "Saurabh", age: 24}
+    // array => [1, 2, 3, 4, 5]
+    // function => function add(a, b) { return a + b }
+
+console.log(typeof "Saurabh"); //string
+console.log(typeof null); //object => bug in JavaScript, null is a primitive data type but typeof null returns object
---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 3839
+let  accountEmail = "Saurabh3839@gmail.com"
+var accountPassword = "12345"
+accountCity = "Una"
+let accountState;
+
+
+// accountId = 2
+
+accountEmail = "abs@gmail.com"
+accountPassword = "54321"
+accountCity = "Mohali"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
